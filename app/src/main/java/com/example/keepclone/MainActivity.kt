ffbf385328149fa070bd5e9943ac420a3f678b9a
sadlerb <sadlerb22@gmail.com>
2021-10-13 02:46:42 -0500

    Added new ToDo Screen

--- app/src/main/java/com/example/keepclone/MainActivity.kt
+++ app/src/main/java/com/example/keepclone/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.keepclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val add_task: FloatingActionButton = findViewById(R.id.floatingActionButton2)
+
+
+        add_task.setOnClickListener {
+            val intent = Intent(this, DisplayAddTaskActivity::class.java)
+            startActivity(intent)
+            val bundle: Bundle? = intent.extras
+        }
+
+
     }
 }
+
+
+
+
+
