7b386a6c20be95f325483e937611de3d8fbb0149
Marlon James <31523311+omisoul@users.noreply.github.com>
2021-10-13 21:20:27 -0500

    Notification intent setup

--- app/src/main/java/com/example/keepclone/MainActivity.kt
+++ app/src/main/java/com/example/keepclone/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.keepclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.notifications, menu)
+
+        val notificationBtn = menu?.findItem(R.id.notification_bell)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.notification_bell-> {
+                Intent(this, Notifications::class.java).also{
+                    startActivity(it)
+                }
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
