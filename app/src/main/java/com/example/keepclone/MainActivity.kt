fcabb0adf179e418259f10623d94afcca55e96da
Shane-Michael Thompson <shane2michael@gmail.com>
2021-10-14 13:20:35 -0500

    got the navbar working now

--- app/src/main/java/com/example/keepclone/MainActivity.kt
+++ app/src/main/java/com/example/keepclone/MainActivity.kt
@@ -2,30 +2,36 @@ package com.example.keepclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.view.Gravity
+import android.view.MenuItem
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val drawerLayout:DrawerLayout = findViewById(R.id.drawer_layout)
+        val nav_view:NavigationView = findViewById(R.id.nav_view)
+        val menu_toggle:ImageButton = findViewById(R.id.menu_toggle)
 
-        val drawer: NavigationView = findViewById(R.id.nav_view);
-        val menu_button: ImageButton = findViewById(R.id.menu_button)
-
-        drawer.visibility= View.INVISIBLE
-        menu_button.setOnClickListener{
-            drawer.visibility= View.VISIBLE
+        // Button to open nav
+        menu_toggle.setOnClickListener{
+            drawerLayout.openDrawer(Gravity.LEFT)
         }
 
-//        val close_button: ImageButton = findViewById(R.id.close)
-//        close_button.setOnClickListener{
-//            drawer.visibility= View.INVISIBLE
-//        }
+        nav_view.setNavigationItemSelectedListener(this)
 
 
-
-
+            }
+    // Get Selected Item
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(applicationContext,"${item.title.toString()} was selected",Toast.LENGTH_SHORT).show()
+        return true
     }
+
 }
+
+
