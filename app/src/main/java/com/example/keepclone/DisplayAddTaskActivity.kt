6f53802e643edc612a356d6aaf354c33c8ed91b0
sadlerb <sadlerb22@gmail.com>
2021-10-13 21:23:21 -0500

    Most functionality is present

--- app/src/main/java/com/example/keepclone/DisplayAddTaskActivity.kt
+++ app/src/main/java/com/example/keepclone/DisplayAddTaskActivity.kt
@@ -1,30 +1,57 @@
 package com.example.keepclone
 
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import java.time.Month
+import java.util.*
 
-class DisplayAddTaskActivity: AppCompatActivity()  {
+class DisplayAddTaskActivity: AppCompatActivity(),DatePickerDialog.OnDateSetListener  {
+    private lateinit var date:String
+    private val datePickerButton:Button = findViewById(R.id.due_date_picker)
+    private val notes:EditText = findViewById(R.id.notes)
+    private val title:EditText = findViewById(R.id.add_todo_text)
+    private val cancel:Button = findViewById(R.id.cancel_button)
+    private val confirm:Button = findViewById(R.id.add_task_button)
+    private val category:Spinner = findViewById(R.id.category_spinner)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_todo)
-        val notes:EditText = findViewById(R.id.notes)
-        val title:EditText = findViewById(R.id.add_todo_text)
-        val cancel:Button = findViewById(R.id.cancel_button)
-        val confirm:Button = findViewById(R.id.add_task_button)
-        val category_spinner:Spinner = findViewById(R.id.category_spinner)
-
-
-
         cancel.setOnClickListener {
             finish()
+        }
+        confirm.setOnClickListener {
+            intent.putExtra("title",title.text.toString())
+            intent.putExtra("notes",notes.text.toString())
+            intent.putExtra("category",category.selectedItem.toString())
+            intent.putExtra("date",date)
+            setResult(Activity.RESULT_OK,intent)
+            finish()
+        }
+
+        datePickerButton.setOnClickListener {
+            val calendar: Calendar = Calendar.getInstance()
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val month = calendar.get(Calendar.MONTH)
+            val year = calendar.get(Calendar.YEAR)
+            val datePickerDialog =
+                DatePickerDialog(this, this, year, month,day)
+            datePickerDialog.show()
         }
 
     }
 
+    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayofMonth: Int) {
+        date = "Date $year/$month/$dayofMonth"
+        datePickerButton.text = date
+
+    }
 
 
 }
--- app/src/main/java/com/example/keepclone/MainActivity.kt
+++ app/src/main/java/com/example/keepclone/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.keepclone
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
@@ -12,17 +14,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val add_task: FloatingActionButton = findViewById(R.id.floatingActionButton2)
+        var notes:String
+        var title:String
+        var category:String
+        var date:String //Format yyyy/mm/dd
+
+        val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                //Data after user chooses to add a task
+                title = result.data?.getStringExtra("title").toString()
+                notes = result.data?.getStringExtra("notes").toString()
+                category = result.data?.getStringExtra("category").toString()
+                date = result.data?.getStringExtra("date").toString()
 
 
-        add_task.setOnClickListener {
-            val intent = Intent(this, DisplayAddTaskActivity::class.java)
-            startActivity(intent)
-            val bundle: Bundle? = intent.extras
+            }
+            else{
+                val toast:Toast = Toast.makeText(this,"Canceled Note",Toast.LENGTH_SHORT)
+            }
         }
-
-
+        //Set FloatingActionButton Listener
+        add_task.setOnClickListener{
+            startForResult.launch(Intent(this,DisplayAddTaskActivity::class.java))
+        }
     }
 }
+
 
 
 
