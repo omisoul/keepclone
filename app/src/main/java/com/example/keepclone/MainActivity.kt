d875929e14d9d41f455a9242d5657dec8429b130
Shane-Michael Thompson <shane2michael@gmail.com>
2021-10-13 22:30:28 -0500

    added necessary code for navbar

--- app/src/main/java/com/example/keepclone/MainActivity.kt
+++ app/src/main/java/com/example/keepclone/MainActivity.kt
@@ -1,33 +1,31 @@
 package com.example.keepclone
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
+import android.widget.ImageButton
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.notifications, menu)
+        val drawer: NavigationView = findViewById(R.id.nav_view);
+        val menu_button: ImageButton = findViewById(R.id.menu_button)
 
-        val notificationBtn = menu?.findItem(R.id.notification_bell)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId){
-            R.id.notification_bell-> {
-                Intent(this, Notifications::class.java).also{
-                    startActivity(it)
-                }
-                return true
-            }
-            else -> super.onOptionsItemSelected(item)
+        drawer.visibility= View.INVISIBLE
+        menu_button.setOnClickListener{
+            drawer.visibility= View.VISIBLE
         }
+
+//        val close_button: ImageButton = findViewById(R.id.close)
+//        close_button.setOnClickListener{
+//            drawer.visibility= View.INVISIBLE
+//        }
+
+
+
+
     }
 }
